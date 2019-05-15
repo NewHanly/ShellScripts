dd731659c5975d53273ee12d19d0b4ecf82ccddc
NewHanly <32947310+NewHanly@users.noreply.github.com>
2019-05-16 00:24:21 +0800

    Create MangaAllinOne.ps1

---
+++ MangaAllinOne.ps1
@@ -0,0 +1,9 @@
+$num=1
+Get-ChildItem | ForEach-Object{
+foreach($ob in (Get-ChildItem $_.Name *.jpeg -Recurse)){
+$filename=[string]$num+'t'+$ob
+Copy-Item  E:\Download\manga-0.5.5_linux\manga_res\$_\origins\$ob -Destination E:\Download\manga-0.5.5_linux\all\$filename
+echo $filename
+}
+$num++
+}
