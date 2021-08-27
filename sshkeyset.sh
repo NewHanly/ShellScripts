f7e94a562ecfbd89ac2ae0e4c58ee7be3b7d97e6
NewHanly <32947310+NewHanly@users.noreply.github.com>
2021-08-27 13:58:27 +0800

    Update sshkeyset.sh

--- sshkeyset.sh
+++ sshkeyset.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-if [ ! -d "/root/.ssh" ]; then
-  mkdir /root/.ssh
+if [ ! -d "~/.ssh" ]; then
+  mkdir ~/.ssh
 fi
 #change this to your sshkey
-echo ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQChhxYKROMK4+QujOPIrzTnqOQqHfgIMiyQJ6PevvDSr7HwhWIm0mmPDJmo6p4XIYJavvfd/Si3AUrnxWY02rTI7XKzd0R6UWDasPhCxnvMD1fHUkM8p5caKUSvPz559pCoxXOyIPv7jkJKCMKttZ5dXP2HAxnZHO8wz0YzayWkh1KQy4+oxVZEcv0l5LB5Z2VmXEpKSAaiow5nMdt+MSNjOgapSaq98v/T7b4eFr3f4ZK9YyvnNrGNCcDcxGKAHqxgYZF5uVxg3CRpTbPQ5+UoaaKzGQwC1Z7o5iPcw4StFdcvr3yKbB3U/AFk75Obuv9O/vVaKnAHZJzGGFOIFLvf >> /root/.ssh/authorized_keys
-chmod 600 /root/.ssh/authorized_keys
-chmod 700 /root/.ssh
-service sshd restart
-if [ ! -f "/root/.ssh/authorized_keys" ]; then
-  echo 'All done'
+echo ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQChhxYKROMK4+QujOPIrzTnqOQqHfgIMiyQJ6PevvDSr7HwhWIm0mmPDJmo6p4XIYJavvfd/Si3AUrnxWY02rTI7XKzd0R6UWDasPhCxnvMD1fHUkM8p5caKUSvPz559pCoxXOyIPv7jkJKCMKttZ5dXP2HAxnZHO8wz0YzayWkh1KQy4+oxVZEcv0l5LB5Z2VmXEpKSAaiow5nMdt+MSNjOgapSaq98v/T7b4eFr3f4ZK9YyvnNrGNCcDcxGKAHqxgYZF5uVxg3CRpTbPQ5+UoaaKzGQwC1Z7o5iPcw4StFdcvr3yKbB3U/AFk75Obuv9O/vVaKnAHZJzGGFOIFLvf >> ~/.ssh/authorized_keys
+chmod 600 ~/.ssh/authorized_keys
+chmod 700 ~/.ssh
+if [ ! -f "~/.ssh/authorized_keys" ]; then
+  echo 'All done,please restart ssh service.'
 fi
